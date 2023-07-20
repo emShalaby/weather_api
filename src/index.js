c16037599de386abe32eb3326984cf9039f96215
emShalaby <galaxian.dev@gmail.com>
2023-07-20 18:52:13 +0300

    remove useless code

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
 import "./style.css";
-import bgImg from "./bg.png";
-const img = document.querySelector("img");
-const cond = document.querySelector("#condition");
-const bg = new Image();
-bg.src = bgImg;
+
 async function getData() {
   const response = await fetch(
     "https://api.weatherapi.com/v1/current.json?key=cca1e056bc40431796805445231907&q=london",
