7e69ea56f0674e6f2d4e40b92f02496aef6b167c
emShalaby <galaxian.dev@gmail.com>
2023-07-20 18:48:18 +0300

    add background image

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
+import "./style.css";
+import bgImg from "./bg.png";
 const img = document.querySelector("img");
 const cond = document.querySelector("#condition");
+const bg = new Image();
+bg.src = bgImg;
 async function getData() {
   const response = await fetch(
     "https://api.weatherapi.com/v1/current.json?key=cca1e056bc40431796805445231907&q=london",
@@ -10,4 +14,5 @@ async function getData() {
   cond.textContent = responseData.current.condition.text;
   img.src = "https:" + responseData.current.condition.icon;
 }
+document.querySelector.body.append(bg);
 getData();
